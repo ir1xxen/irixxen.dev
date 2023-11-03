f476220041e367211b4e8dcefc8316ab272ac25a
irixxen <147881659+ir1xxen@users.noreply.github.com>
2023-11-03 09:58:16 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="slider">
+    <section>
+      <h1>vlapsk1y</h1>
+      <div className="nicknames">
+        <span>also el-psy-congroo, 0x7d5</span>
+      </div>
+    </section>
+    <section>
+      <h1>A member</h1>
+      <div className="list">
+      <span>of <a href="https://razoomart.ru">razoom.art</a> as almost full-stack dev & devops</span>
+      </div>
+    </section>
+    <section>
+      <h1>About me</h1>
+      <div className="nicknames">
+      <span>I am self-taught software developer (Swift, Java, C# and etc).<br/>You can find more info on my github.</span>
+      </div>
+    </section>
+    <section>
+      <h1>Contact me</h1>
+      <div className="list">
+        <a href="https://vk.com/vlapskiy">VK</a>
+        <a href="https://github.com/vlapsk1y">Github</a>
+        <a href="https://vlapsk1y.t.me/">Telegram</a>
+        <a href="https://discordapp.com/users/993264079043035269">Discord</a>
+        <a href="https://mastodon.ml/@vlpask1y">Mastodon</a>
+      </div>
+    </section>
+  </div>
+  );
+}
+
+export default App;
